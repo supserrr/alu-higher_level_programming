ed155fc6c36a777cda9fa4f0d13bff15b1aa9b8c
supserrr <s.shima@alustudent.com>
2024-10-23 12:46:20 -0700

    Created and tested scripts ensuring that they all meet project requirements and are properly formatted.

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-# why does it need to be 8 lines? To give space to use variables. Still not doing it the "intended way", screw you pep8
-[first, last, middle] = [word[:3], word[-2:], word[1:-1]]
-
-print(f"First 3 letters: {first}")
-print(f"Last 2 letters: {last}")
-print(f"Middle word: {middle}")
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
