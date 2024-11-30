d67839944ad58b9611dd72cb30abc00ad734a249
supserrr <s.shima@alustudent.com>
2024-11-30 04:29:43 +0200

    Updated 13-model_state_delete_a.py

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
 """
-Script that deletes all State objects with a name containing
-the letter a from the database
-Using module SQLAlchemy
+Deletes all State objects with a name containing
+the letter 'a' from the database.
 """
 
-from model_state import Base, State
 from sqlalchemy import create_engine
+from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    # create an engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    # create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-    # create a Session
-    session = Session()
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbName = sys.argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            username,
+            password,
+            dbName
+        ),
+        pool_pre_ping=True,
+    )
     Base.metadata.create_all(engine)
-    state_del = session.query(State).filter(State.name.like('%a%')).all()
-    for delete in state_del:
-        session.delete(delete)
-    # commit and close session
+    Session = sessionmaker(engine)
+
+    session = Session()
+
+    session.query(State).filter(State.name.like("%a%")).\
+        delete(synchronize_session='fetch')
     session.commit()
+
+    session.close()
     session.close()
