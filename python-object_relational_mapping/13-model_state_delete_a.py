5ba49f2bdb78129fdef77b2cd8ca8e806b20f9d1
supserrr <s.shima@alustudent.com>
2024-11-30 04:32:17 +0200

    Updated 13-model_state_delete_a.py

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
 """
-Deletes all State objects with a name containing
-the letter 'a' from the database.
+Module to get all states
 """
+from sys import argv
 
+from model_state import State, Base
 from sqlalchemy import create_engine
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-import sys
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    dbName = sys.argv[3]
-
+    username, password, database = argv[1:4]
+    # default host is 'localhost' and default port is '3306'
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            username,
-            password,
-            dbName
-        ),
-        pool_pre_ping=True,
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(username, password, database),
+        pool_pre_ping=True
     )
+    session = Session(engine)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(engine)
 
-    session = Session()
+    states = session.query(State).filter(
+        State.name.contains('a')
+    ).all()
+    for state in states:
+        session.delete(state)
+    session.commit()  # why it failed, i forgot to commit the changes
 
-    session.query(State).filter(State.name.like("%a%")).\
-        delete(synchronize_session='fetch')
-    session.commit()
-
-    session.close()
     session.close()
